0e6f3621194a639b54e767fa044d8f4b0596edb4
AltESV <ishigami.daniel@gmail.com>
2023-03-15 16:50:22 +0000

    navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
+import Navbar from '../components/Navbar';
 import './Home.css'
+import bannerAnimals from '../assets/bannerAnimals.png'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
-    
-  )
+    <div className='home'>
+     <Navbar/>
+    <img src={bannerAnimals} />
+    <h1>Animals</h1>
+    <p>From Africa to Antartica, animal charities work to support wildlife, their habitats, and the local people</p>
+    </div>
+  );
 }
+
+
